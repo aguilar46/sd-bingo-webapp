45b6217511631873e5526bf392aa92affaf013f2
aguilar46 <aguilar46@gmail.com>
2024-10-13 20:23:35 -0500

    Added random rotation to bingo mark.

--- src/components/BingoSpace.js
+++ src/components/BingoSpace.js
@@ -3,7 +3,7 @@
  * Created On: 2021-01-15T05:35:26.505Z
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import _ from 'lodash';
 import redMark from '../images/red_dab.svg';
@@ -24,12 +24,23 @@ const Space = styled.button`
   height: 100%;
   justify-content: center;
   align-items: center;
-
-  ${(props) => props.selected && `background-image: url(${props.imageUrl});`}
-  background-size: 100%;
-  background-repeat: no-repeat;
-  background-position: center;
   background-color: transparent;
+  overflow: hidden;
+`;
+
+const BackgroundDiv = styled.div`
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  background: url(${(props) => props.selected && props.imageUrl}) 0 0 no-repeat;
+  transform: rotate(${(props) => props.rotation}turn);
+  background-position: center;
+`;
+
+const LabeDiv = styled.div`
+  transform: rotate(${(props) => props.rotation}turn);
+  height: 100%;
+  align-content: center;
 `;
 
 const markImages = {
@@ -42,17 +53,25 @@ const markImages = {
 };
 
 const BingoSpace = (props) => {
-  const { label } = props;
+  const { label, selected } = props;
   const markColor = useAtomValue(atoms.markColor);
   const image = markImages[markColor];
+  const [rotation, setRotation] = useState(0);
+
+  useEffect(() => {
+    setRotation(Math.random());
+  }, []);
+
   return (
-    <Space
-      {..._.omit(props, ['label'])}
-      className="bingo-space"
-      imageUrl={image}
-      title={label}
-    >
-      {label}
+    <Space {..._.omit(props, ['label'])} className="bingo-space" title={label}>
+      <BackgroundDiv
+        className="background-div"
+        selected={selected}
+        imageUrl={image}
+        rotation={rotation}
+      >
+        <LabeDiv rotation={1 - rotation}>{label}</LabeDiv>
+      </BackgroundDiv>
     </Space>
   );
 };
